9ea9c4e58939b10815f06e15a6c8e72ba9584e54
Juan Arias <juan.arias.botella@gmail.com>
2022-06-21 20:49:31 +0200

    Update GameManager

--- Assets/GameAssets/Scritps/Manager/GameManager.cs
+++ Assets/GameAssets/Scritps/Manager/GameManager.cs
@@ -9,21 +9,85 @@ public struct LevelData
     public string m_levelName;
 }
 
+public enum GameState
+{
+    MainMenu,
+    Playing,
+    Pause,
+    Victory,
+    Lose,
+    Exit
+}
+
 public class GameManager : PersistentSingleton<GameManager>
 {
     public Player m_player;
     public LevelData m_currentLevel;
+    public GameState m_gameState;
 
     // Start is called before the first frame update
     void Start()
     {
         MapLoader.LoadMap(0);
+        UpdateGameState(GameState.MainMenu);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void HandleVictory()
     {
-                    
+        // Cargar pantalla de victoria
     }
 
+    private void HandleLose()
+    {
+        // Cargar pantalla de derrota
+    }
+
+    private void HandleMainMenu()
+    {
+        // Cargar pantalla de menu principal
+    }
+
+    private void HandlePlaying()
+    {
+        Application.Quit();
+    }
+    
+    private void HandlePause()
+    {
+        Time.timeScale = 0;
+    }
+    
+    private void HandleExit()
+    {
+        Time.timeScale = 0;
+    }
+
+    public void UpdateGameState(GameState _gameState)
+    {
+        m_gameState = _gameState;
+
+        switch (m_gameState)
+        {
+            case GameState.MainMenu:
+                HandleMainMenu();
+                break;
+            case GameState.Playing:
+                HandlePlaying();
+                break;
+            case GameState.Pause:
+                HandlePause();
+                break;
+            case GameState.Victory:
+                HandleVictory();
+                break;
+            case GameState.Lose:
+                HandleLose();
+                break;
+            case GameState.Exit:
+                HandleExit();
+                break;
+            default:
+                break;
+        }
+    }
 }
