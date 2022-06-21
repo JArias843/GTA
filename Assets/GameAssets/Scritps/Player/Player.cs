1e60ddf661b62440defb3565f0266654d4c11a73
Adayer <aday3r@gmail.com>
2022-06-21 16:41:20 +0200

    New Input System

--- Assets/GameAssets/Scritps/Player/Player.cs
+++ Assets/GameAssets/Scritps/Player/Player.cs
@@ -1,9 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+    //Controls
+    [SerializeField] private InputActionMap m_controls;
+
+    //Movement
+    [SerializeField] private float m_maxSpeed = 10.f;
+    [SerializeField] private float m_acceleration = 2.f;
+
+    private void OnEnable()
+    {
+
+    }
+
     // Start is called before the first frame update
     void Start()
     {
